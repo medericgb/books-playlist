7e94321d56cf1d9c7f448527f33078cc71874ea9
Mederic <medericjoel@gmail.com>
2020-06-28 14:01:47 +0000

    Dummy data

---
+++ backend/data.js
@@ -0,0 +1,16 @@
+// // Dummy datas
+// export const books = [
+//     {name: "Name of wind", gender: "Fantasy", id: "1", authorId: "1"},
+//     {name: "The Final Empire", gender: "Fantasy", id: "2", authorId: "2"},
+//     {name: "Le Long Earth", gender: "Sci-Fi", id: "3", authorId: "1"},
+//     {name: "The Hero of Age", gender: "Romantic", id: "4", authorId: "3"},
+//     {name: "The Color of Magic", gender: "Sci-Fi", id: "5", authorId: "2"},
+//     {name: "The Light fantasy", gender: "Fantasy", id: "6", authorId: "3"},
+//     {name: "Only thing", gender: "Romantic", id: "7", authorId: "3"},
+// ]
+
+// export const authors = [
+//     { name: "Patrick Rothfuss", age: 44, id: "1"},
+//     { name: "Brandon Sanderson", age: 42, id: "2"},
+//     { name: "Terry Pratchett", age: 66, id: "3"}
+// ]
