34c127e72ec756b70e82e27852833447e22efcf6
medericgb <46417515+medericgb@users.noreply.github.com>
2020-07-11 10:13:17 +0000

    Update AddBook.js

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React,{ useState } from 'react';
 import { graphql, Query, useMutation } from 'react-apollo';
 import {flowRight as compose} from 'lodash';
 
